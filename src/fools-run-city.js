78517a5ca90b40f49b2f9187cd9ca21f53b32f35
Arlo Belshee <arlo.belshee@live.com>
2017-11-18 17:30:12 -0800

    Now I'm showing the message by sending a message of a well-defined type to the User district, invoking a well-known display handler.

--- src/fools-run-city.js
+++ src/fools-run-city.js
@@ -1,3 +1,41 @@
 
+var District = function () {
+	function District(name) {
+		var self = this;
+		self.name = name;
+		self.routes = new Map();
+		self.add_message_handler = function add_message_handler(message_type, handler) {
+			if (!self.routes.has(message_type)) {
+				self.routes.set(message_type, []);
+			}
+			self.routes.get(message_type).push(handler);
+		};
+		self.send = function send(message) {
+			var handlers = self.routes.get(message.constructor);
+			if (!handlers) return;
+			for (idx in handlers) {
+				handlers[idx](message);
+			}
+		};
+	}
+	District.static_fn = function () {
+	};
+	return District;
+}();
 
-console.log("hello, world");
+var districts = {
+	user: new District("user"),
+};
+
+function Show(message) {
+		var self = this;
+		self.message = message;
+};
+
+function display_message(show) {
+	console.log(show.message);
+}
+
+districts.user.add_message_handler(Show, display_message);
+
+districts.user.send(new Show("hello, world"));
